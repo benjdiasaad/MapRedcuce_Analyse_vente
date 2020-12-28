e49d01e30d73a517ee77b39bb305b1e93b4741e9
benjdia <cravatas90@gmail.com>
2020-12-28 12:56:10 +0100

    Add all necessary jar files

---
+++ AnalyseVente/src/org/saad/analyse/vente/analyseVente.java
@@ -0,0 +1,32 @@
+package org.saad.analyse.vente;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.hadoop.io.SequenceFile;
+//import org.apache.hadoop.io.file.*;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class analyseVente{
+	
+}
